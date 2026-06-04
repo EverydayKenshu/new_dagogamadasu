2ac96b1b67924ef53b1c5900e831c7345a839de0
SysTrustYada <yada.systemtrust@gmail.com>
2026-06-04 17:42:22 +0900

    メインから

--- src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
+++ src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
@@ -10,6 +10,10 @@ import jp.co.sss.shop.repository.ItemRepository;
 import jp.co.sss.shop.service.BeanTools;
 
 /**
+ * めいんから
+ */
+
+/**
  * 商品管理 一覧表示機能(一般会員用)のコントローラクラス
  *
  * @author SystemShared
